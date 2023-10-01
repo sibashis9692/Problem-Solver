c51e99f66f9774e43fb2ed1f2a484cf95950d902
sibashis Nayak <sibashisnayak228@gmail.com>
2023-10-01 13:27:46 +0530

    Fix bugs

--- problemsolver/static/javascript/signup.js
+++ problemsolver/static/javascript/signup.js
@@ -1,8 +1,3 @@
-function submitForm(){
-    const form = document.querySelector('form');
-    form.submit()
-}
-
 function getSelectedOption() {
     var selectElement = document.getElementById("mySelect");
     var selectedOption = selectElement.options[selectElement.selectedIndex].value;
